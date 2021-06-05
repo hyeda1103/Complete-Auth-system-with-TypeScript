43cfb4f5d83afbb686eb07519d730990da19600e
hyeda1103 <dalgona92@gmail.com>
2021-06-06 02:25:54 +0900

    Change Message when email is not validated

--- backend/src/validators/auth.ts
+++ backend/src/validators/auth.ts
@@ -7,11 +7,10 @@ export const userSignUpValidator = [
         .withMessage('이름을 입력해주세요'),
     check('email')
         .isEmail()
-        .withMessage('이메일 주소를 입력해주세요'),
+        .withMessage('이메일 주소가 유효하지 않습니다'),
     check('password')
-        .isLength({min: 6})
-        .withMessage('비밀번호는 최소 6자 이상이어야 합니다')
-]
+        .isLength({ min: 6 })
+        .withMessage('비밀번호는 최소 6자 이상이어야 합니다')]
 
 export const userSignInValidator = [
     check('email')
