1a869171c00fbb4079848805f195704644ac08b6
hyeda1103 <dalgona92@gmail.com>
2021-06-06 16:39:08 +0900

    Create loading Page

--- frontend/src/components/common/Loader.tsx
+++ frontend/src/components/common/Loader.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+interface IProps {
+  children: string | undefined
+}
 
 const Loader = () => {
-  return <h1>Loading...</h1>
+  return <MsgBlock>로딩중입니다</MsgBlock>
 }
 
 export default Loader
+
+const MsgBlock = styled.div`
+  border: 1px solid ${({ theme }) => theme.text};
+  border-radius: 5px;
+  padding: 0.5rem;
+`
