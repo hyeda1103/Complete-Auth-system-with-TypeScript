63a124cca6f0daa253e74667afaf006b5071f90d
hyeda1103 <dalgona92@gmail.com>
2021-06-06 16:40:19 +0900

    Adjust nav Items if userInfo exists

--- frontend/src/components/common/SideBar.tsx
+++ frontend/src/components/common/SideBar.tsx
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { closeSideBar } from "../../modules/sideBar";
+import { signout } from "../../modules/user";
+import { RootState } from '../../modules'
 
-type Props = {
+interface IProps {
   open: boolean;
 };
 
-const SideBar = ({ open }: Props) => {
+const SideBar = ({ open }: IProps) => {
   const dispatch = useDispatch(); // 디스패치 함수를 가져옵니다
   const onClick = () => {
     dispatch(closeSideBar()) ;
   };
+  
+  const userSignIn = useSelector((state: RootState) => state.signIn)
+  const { loading, error, userInfo } = userSignIn
+
+  const logoutHandler = () => {
+    dispatch(signout());
+    document.location.href = "/signin";
+  };
+
   return (
     <Nav open={open}>
-      <NavItem to="/" onClick={onClick}>
-        로그아웃
-      </NavItem>
+      {userInfo ? (
+        <Fragment>
+          <NavItemNoLink onClick={onClick}>
+            {userInfo.name}의 노션
+          </NavItemNoLink>
+          <NavItemNoLink onClick={logoutHandler}>로그아웃</NavItemNoLink>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <NavItem to="/signup" onClick={onClick}>
+            회원가입
+          </NavItem>
+          <NavItem to="/signin" onClick={onClick}>
+            로그인
+          </NavItem>
+        </Fragment>
+      )}
     </Nav>
   );
 };
 
-export default React.memo(SideBar);
+export default SideBar;
 
-const Nav = styled.section<Props>`
+const Nav = styled.section<IProps>`
   height: calc(100vh - 60px);
   top: 60px;
   transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
@@ -38,7 +63,16 @@ const Nav = styled.section<Props>`
 const NavItem = styled(Link)`
   text-decoration: none;
   font-size: 18px;
-  font-weight: 300;
+  padding: 15px 20px;
+  width: 100%;
+  letter-spacing: 1.4px;
+  cursor: pointer;
+  display: block;
+  color: #f6f5f0;
+`;
+
+const NavItemNoLink = styled.span`
+  font-size: 18px;
   padding: 15px 20px;
   width: 100%;
   letter-spacing: 1.4px;
