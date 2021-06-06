81564de0bb91ad3ba69857776914a0273b0416ab
hyeda1103 <dalgona92@gmail.com>
2021-06-07 00:54:57 +0900

    Add forgotValidator&resetPasswordValidator

--- backend/src/validators/auth.ts
+++ backend/src/validators/auth.ts
@@ -20,3 +20,18 @@ export const userSignInValidator = [
         .isLength({ min: 6 })
         .withMessage('비밀번호는 최소 6자 이상이어야 합니다')]
 
+export const forgotPasswordValidator = [
+    check('email')
+        .not()
+        .isEmpty()
+        .isEmail()
+        .withMessage('이메일 주소가 유효하지 않습니다')
+]
+
+export const resetPasswordValidator = [
+    check('newPassword')
+        .not()
+        .isEmpty()
+        .isLength({ min: 6 })
+        .withMessage('비밀번호는 최소 6자 이상이어야 합니다')
+]
