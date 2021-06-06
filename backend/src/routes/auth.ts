12e6a9ba8b87d1dc74d89502859d4f04395e70b0
hyeda1103 <dalgona92@gmail.com>
2021-06-07 00:22:33 +0900

    Add routes to /forgot-password & /reset-password

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 // import controller
-import { signup, signin, accountActivation } from '../controllers/auth'
+import { signup, signin, accountActivation, forgotPassword, resetPassword, closeAccount } from '../controllers/auth'
 // import validators
-import { userSignInValidator, userSignUpValidator } from './../validators/auth';
+import { userSignInValidator, userSignUpValidator, forgotPasswordValidator, resetPasswordValidator } from './../validators/auth'
 import { runValidation } from './../validators/index';
+import { protect } from './../middleware/auth'
 
 const router = express.Router()
 
@@ -12,5 +13,8 @@ router
     .post(userSignUpValidator, runValidation, signup)
 router.post('/account-activation', accountActivation)
 router.post('/signin', userSignInValidator, runValidation, signin)
+router.put('/forgot-password', forgotPasswordValidator, runValidation, forgotPassword)
+router.put('/reset-password', resetPasswordValidator, runValidation, resetPassword)
+router.route('/:id').delete(protect, closeAccount)
 
 export { router }
