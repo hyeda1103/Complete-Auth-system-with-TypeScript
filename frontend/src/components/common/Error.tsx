5d015f57810e37843bc6c9d69ccc9ee470ea4d5b
hyeda1103 <dalgona92@gmail.com>
2021-06-07 17:42:33 +0900

    Create Error Component

---
+++ frontend/src/components/common/Error.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface IProps {
+  children: string | undefined
+}
+
+const Error = ({ children }: IProps) => {
+  return <MsgForm>{children}</MsgForm>
+}
+
+export default Error
+
+const MsgForm = styled.div`
+  border: 1px solid ${({ theme }) => theme.text};
+  border-radius: 5px;
+  padding: 0.5rem;
+`
